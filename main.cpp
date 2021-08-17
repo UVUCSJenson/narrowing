95de12f66bf224de4b1e389985efe2ba18c13894
Ken Jenson <kenneth.jenson@gmail.com>
2021-08-17 20:55:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 3 / 2;
+    auto m = 3 / 2.0;
+    int k = 3 / 2.0;
+    cout << n << ", " << m << ", " << k << endl;
+}
+
+/* Output:
+1, 1.5, 1
+*/
